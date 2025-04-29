df7520e7dc82d9a60e05971292be22126d4f4973
skye <67734042+sky-larke@users.noreply.github.com>
2025-04-28 19:34:48 -0500

    Page2: Added basic analysis & helpers
    - added avg price per category
    - avg rank per category
    - avg price for ‘rank range’

--- tea_tracker/app/controllers/teas_controller.rb
+++ tea_tracker/app/controllers/teas_controller.rb
@@ -1,6 +1,26 @@
 class TeasController < ApplicationController
     def show
         @tea = Tea.find(params[:id])
+
+        @this_category = Tea.where(category: @tea.category)
+        category_comparator = TeasHelper::RankComparator.new(@tea, @this_category)
+        @category_rank_comparison = category_comparator.compare
+
+        @closest_price_teas = Tea
+            .where.not(id: @tea.id)
+            .order(Arel.sql("ABS(price - #{ActiveRecord::Base.connection.quote(@tea.price)})"))
+            .limit(3)
+
+        price_comparator = TeasHelper::RankComparator.new(@tea, @closest_price_teas)
+        @price_rank_comparison = price_comparator.compare
+
+        @closest_rank_teas = Tea
+            .where.not(id: @tea.id)
+            .order(Arel.sql("ABS(rank - #{ActiveRecord::Base.connection.quote(@tea.rank)})"))
+            .limit(3)
+        rank_comparator = TeasHelper::RankComparator.new(@tea, @closest_rank_teas, "price")
+        @rank_price_comparison = rank_comparator.compare
+
     end
 end
     
