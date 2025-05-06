6803242ad7ab190c3bce012b82234586b521c46e
Brighton Sibanda <brightonsibanda2025@u.northwestern.edu>
2025-05-06 11:04:25 -0500

    View page styling

--- tea_tracker/app/controllers/home_controller.rb
+++ tea_tracker/app/controllers/home_controller.rb
@@ -3,7 +3,22 @@ class HomeController < ApplicationController
 
   def index
     if current_user
-      @teas = current_user.teas
+     
+      @categories = current_user.teas.select(:category).distinct.pluck(:category)
+      @sort = params[:sort]
+      @filter = params[:filter]
+      @teas = Tea.where(user_id: current_user.id)
+      @teas = @teas.where(category: @filter) if @filter.present?
+      @teas = @teas.order(price: :asc) if @sort == "price_asc"
+      @teas = @teas.order(price: :desc) if @sort == "price_desc"
+
+      @most_common_category = Tea.where(user_id: current_user.id)
+                              .group(:category)
+                              .order('count_id DESC')
+                              .count(:id)
+                              .first&.first
+
+      @average_price = @teas.average(:price)
       render :dashboard
     else
       render :landing
