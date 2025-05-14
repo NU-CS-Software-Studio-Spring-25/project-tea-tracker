4d386a126d79bf9d6761e16407e09082c0a7c11c
aidenvandenbush <aiden.vandenbush@gmail.com>
2025-05-14 12:13:36 -0500

    fixed user tea association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :entries, dependent: :destroy
+  has_many :teas, through: :entries
   has_secure_password
 
   validates :username, presence: true, uniqueness: true
