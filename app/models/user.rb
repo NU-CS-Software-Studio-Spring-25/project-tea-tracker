15233e165c4aa01d2510a28d174d8108105f3eba
aidenvandenbush <aiden.vandenbush@gmail.com>
2025-05-14 11:55:41 -0500

    added error handling for create new user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
-    has_many :entries, dependent: :destroy
-    has_many :teas, through: :entries
-    has_secure_password
+  has_many :entries, dependent: :destroy
+  has_secure_password
 
-    validates :username, presence: true, uniqueness: true
-    validates :password, length: { minimum: 6 }, if: :password_digest_changed?
+  validates :username, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 8, message: "must be at least 8 characters long" }
+  validates :password_confirmation, presence: true
 end
